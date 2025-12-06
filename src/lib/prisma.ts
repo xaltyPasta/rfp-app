a45c46624f28cb9bfdadab7a1529b349498dbf0e
xaltyPasta <rajkumarravi9470@gmail.com>
2025-12-07 02:27:57 +0530

    schema prisma and client added

---
+++ src/lib/prisma.ts
@@ -0,0 +1,19 @@
+// src/lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var __prisma: PrismaClient | undefined;
+}
+
+const prisma =
+  global.__prisma ??
+  new PrismaClient({
+    log: ["query", "info", "warn", "error"], // optional but helpful in dev
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  global.__prisma = prisma;
+}
+
+export default prisma;
